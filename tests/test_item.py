119a3ca2230f99fc707e223ec772eec97e4349e0
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-28 18:55:51 +0300

    Add test_change_name, add docstring Rename test__repr__, test_name

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,25 +2,30 @@ import pytest
 from shop.item import Item
 
 
-def test___repr__(item1):
-    assert Item.__repr__(item1) == "Item(Смартфон, 10000, 20)"
+def test_get_item(item1):
+    """Проверка инициализации экземпляра Item"""
+    assert repr(item1) == "Item(Смартфон, 10000, 20)"
 
 
 def test__str__(item2):
-    assert Item.__str__(item2) == "Ноутбук"
+    """Проверка вывода информации для пользователя"""
+    assert str(item2) == "Ноутбук"
 
 
 def test_calculate_total_price(item1):
+    """Ожидается общая стоимость конкретного товара в магазине"""
     assert Item.calculate_total_price(item1) == 200000
 
 
 def test_apply_discount(item1, item2):
+    """Ожидается применение установленной скидки для конкретного товара"""
     item1.apply_discount()
     assert item1.price == 8500
     assert item2.price == 20000
 
 
 def test_instantiate_from_csv():
+    """Ожидается инициализация экземпляра Item из файла .csv"""
     Item.all = []
     Item.path_to_file_csv = 'tests/test.csv'
     Item.instantiate_from_csv()
@@ -32,26 +37,35 @@ def test_instantiate_from_csv():
 
 
 def test_is_integer():
-    assert Item.is_integer(5.0) is True
+    """Проверка, является ли число целым, ожидается True или False"""
+    assert Item.is_integer(5) is True
     assert Item.is_integer(5.0) is True
     assert Item.is_integer(5.5) is False
-    assert Item.is_integer('5.0') is True
+    assert Item.is_integer('5') is True
     assert Item.is_integer('5.0') is True
     assert Item.is_integer('5.5') is False
 
 
 def test_get_int():
+    """Ожидается int из str"""
     assert Item.get_int('5') == 5
     assert Item.get_int('5.7') == 5
     assert Item.get_int('5.0') == 5
 
 
-def test_name(item2):
+def test_get_name(item2):
+    """Ожидается получение значения name"""
     assert item2.name == 'Ноутбук'
+
+
+def test_change_name(item2):
+    """Ожидается замена значения name"""
     item2.name = 'Смартфон'
     assert item2.name == 'Смартфон'
 
 
 def test_exception_name(item1):
+    """Ожидается исключение Exception, если name более 10 символов """
     with pytest.raises(Exception):
         item1.name = 'Длина наименования товара превышает 10 символов.'
+
